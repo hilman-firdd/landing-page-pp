7e4e6819b97057e2af971d1887f60de972008c0a
hilman-firdd <hilmanfirdaus48@gmail.com>
2022-10-08 13:55:38 +0700

    bug-logo-page

--- js/script.js
+++ js/script.js
@@ -1,55 +1,50 @@
-/**
- * WEBSITE: https://themefisher.com
- * TWITTER: https://twitter.com/themefisher
- * FACEBOOK: https://www.facebook.com/themefisher
- * GITHUB: https://github.com/themefisher/
- */
-
 (function ($) {
-  'use strict';
+  "use strict";
 
   /* ========================================================================= */
   /*	Page Preloader
   /* ========================================================================= */
-  $(window).on('load', function () {
-    $('#preloader').fadeOut('slow', function () {
+  $(window).on("load", function () {
+    $("#preloader").fadeOut("slow", function () {
       $(this).remove();
     });
   });
 
-
   // navbarDropdown
-	if ($(window).width() < 992) {
-		$('#navigation .dropdown-toggle').on('click', function () {
-			$(this).siblings('.dropdown-menu').animate({
-				height: 'toggle'
-			}, 300);
-		});
+  if ($(window).width() < 992) {
+    $("#navigation .dropdown-toggle").on("click", function () {
+      $(this).siblings(".dropdown-menu").animate(
+        {
+          height: "toggle",
+        },
+        300
+      );
+    });
   }
-  
+
   //Hero Slider
-  $('.hero-slider').slick({
+  $(".hero-slider").slick({
     autoplay: true,
     infinite: true,
     arrows: true,
-    prevArrow: '<button type=\'button\' class=\'prevArrow\'></button>',
-    nextArrow: '<button type=\'button\' class=\'nextArrow\'></button>',
+    prevArrow: "<button type='button' class='prevArrow'></button>",
+    nextArrow: "<button type='button' class='nextArrow'></button>",
     dots: false,
     autoplaySpeed: 7000,
     pauseOnFocus: false,
-    pauseOnHover: false
+    pauseOnHover: false,
   });
-  $('.hero-slider').slickAnimation();
+  $(".hero-slider").slickAnimation();
 
   /* ========================================================================= */
   /*	Portfolio Filtering Hook
   /* =========================================================================  */
   // filter
-  setTimeout(function(){
-    var containerEl = document.querySelector('.filtr-container');
+  setTimeout(function () {
+    var containerEl = document.querySelector(".filtr-container");
     var filterizd;
     if (containerEl) {
-      filterizd = $('.filtr-container').filterizr({});
+      filterizd = $(".filtr-container").filterizr({});
     }
   }, 500);
 
@@ -57,81 +52,82 @@
   /*	Testimonial Carousel
   /* =========================================================================  */
   //Init the slider
-  $('.testimonial-slider').slick({
+  $(".testimonial-slider").slick({
     infinite: true,
     arrows: false,
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 2000,
   });
-
 
   /* ========================================================================= */
   /*	Clients Slider Carousel
   /* =========================================================================  */
   //Init the slider
-  $('.clients-logo-slider').slick({
+  $(".clients-logo-slider").slick({
     infinite: true,
     arrows: false,
     autoplay: true,
     autoplaySpeed: 2000,
     slidesToShow: 5,
     slidesToScroll: 1,
-    responsive: [{
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: false
-      }
-    }
-    ]
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: false,
+        },
+      },
+    ],
   });
 
   /* ========================================================================= */
   /*	Company Slider Carousel
   /* =========================================================================  */
-  $('.company-gallery').slick({
+  $(".company-gallery").slick({
     infinite: true,
     arrows: false,
     autoplay: true,
     autoplaySpeed: 2000,
     slidesToShow: 5,
     slidesToScroll: 1,
-    responsive: [{
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 667,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: false
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        arrows: false
-      }
-    }
-    ]
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 667,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: false,
+        },
+      },
+    ],
   });
 
   /* ========================================================================= */
@@ -144,27 +140,30 @@
   // -----------------------------
   function counter() {
     var oTop;
-    if ($('.counter').length !== 0) {
-      oTop = $('.counter').offset().top - window.innerHeight;
+    if ($(".counter").length !== 0) {
+      oTop = $(".counter").offset().top - window.innerHeight;
     }
     if ($(window).scrollTop() > oTop) {
-      $('.counter').each(function () {
+      $(".counter").each(function () {
         var $this = $(this),
-          countTo = $this.attr('data-count');
+          countTo = $this.attr("data-count");
         $({
-          countNum: $this.text()
-        }).animate({
-          countNum: countTo
-        }, {
-          duration: 1000,
-          easing: 'swing',
-          step: function () {
-            $this.text(Math.floor(this.countNum));
+          countNum: $this.text(),
+        }).animate(
+          {
+            countNum: countTo,
           },
-          complete: function () {
-            $this.text(this.countNum);
+          {
+            duration: 1000,
+            easing: "swing",
+            step: function () {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function () {
+              $this.text(this.countNum);
+            },
           }
-        });
+        );
       });
     }
   }
@@ -176,10 +175,9 @@
 
     var scroll = $(window).scrollTop();
     if (scroll > 50) {
-      $('.navigation').addClass('sticky-header');
+      $(".navigation").addClass("sticky-header");
     } else {
-      $('.navigation').removeClass('sticky-header');
+      $(".navigation").removeClass("sticky-header");
     }
   });
-
 })(jQuery);
